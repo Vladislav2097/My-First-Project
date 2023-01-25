4c717259e61267e88ea2143a025a4db66ed7edbd
Vladislav2097 <vladislav2097@mail.ru>
2023-01-25 13:59:47 +0400

    fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import genDiff from '../formatters/index.js';
+import genDiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
