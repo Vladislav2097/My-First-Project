6796fd2fab09126424d861bbcfdac09632aa92fe
Vladislav2097 <vladislav2097@mail.ru>
2023-01-30 10:08:13 +0400

    fixes

--- src/parsers.js
+++ src/parsers.js
@@ -3,16 +3,16 @@ import yaml from 'js-yaml';
 const parse = (data, format) => {
   if (format === 'json') {
     return JSON.parse(data);
-  } 
-  
+  }
+
   if (format === 'yml') {
     return yaml.load(data);
-  } 
-  
+  }
+
   if (format === 'yaml') {
     return yaml.load(data);
   }
-  
+
   return 'Eror';
 };
 
